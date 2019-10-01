370c0dce1f64206ff44ae6d7f75b5b7581afb18a
Vladislav Nikolov <vladis890@gmail.com>
2019-10-01 20:54:42 +0200

    Finished third subsection of parking model.

--- P1/vis/aparc-3d.py
+++ P1/vis/aparc-3d.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-y_c = np.array([99, 99, 99, 99, 99, 98, 98, 98, 97, 94, 36])
-x_prob = np.array([0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99])
-z_dist = np.array([0.008400, 0.100000, 0.213100, 0.344800, 0.495100, 0.744800, 1.074900, 1.659600, 2.901000, 6.477300, 67.957802 ])
+y_c = np.array([94, 94, 94, 93, 92, 90, 86, 83, 82, 72, 80])
+x_vis = np.array([2, 4, 6, 8, 10, 15, 20, 25, 30, 50, 100])
+z_dist = np.array([6.480800, 6.339700, 6.069100, 5.706100, 5.505300, 5.244000, 4.954400, 4.890900, 4.778700, 4.687100, 4.624000])
 
-ax.set_xlabel('Probabilidad de ocupacion')
+ax.set_xlabel('Alcance de visión')
 ax.set_ylabel('Posicion inicial')
 ax.set_zlabel('Distancia media al destino')
 
-ax.set_zlim(0, 100)
+ax.set_zlim(0, 7)
 
-ax.plot3D(x_prob, y_c, z_dist, 'r')
-ax.scatter3D(x_prob, y_c, z_dist, 'b')
+ax.plot3D(x_vis, y_c, z_dist, 'r')
+ax.scatter3D(x_vis, y_c, z_dist, 'b')
 
+print(y_c)
+print(x_vis)
 plt.show()
