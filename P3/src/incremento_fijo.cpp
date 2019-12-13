1a4ccfbfa5de836c89ea4b0df60ec8854a1594c1
Vladislav Nikolov <vladis890@gmail.com>
2019-12-13 10:40:37 +0100

    Añade archivos iniciales P3

---
+++ P3/src/incremento_fijo.cpp
@@ -0,0 +1,179 @@
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+
+bool servidor_libre;
+double acum_cola, inicio_ocio, ocio, tLlegada, tServicio;
+long long int atendidos, encola, infinito, numSimul, reloj, tiempo_llegada,
+              tiempo_salida, total_a_atender, tultsuc;
+
+float generaLlegada(float tLlegada)
+{
+    // Generar valor aleatorio y redondearlo
+	float u = (float) ((float) random()/(RAND_MAX+1.0));
+	u = ceil(-tLlegada*log(1-u));
+
+	return u != 0 ? u : 1.0;
+}
+
+float generaServicio(float tServicio)
+{
+    // Generar valor aleatorio y redondearlo
+	float u = (float) ((float) random()/(RAND_MAX+1.0));
+	u = ceil(-tServicio*log(1-u));
+
+    return u != 0 ? u : 1.0;
+}
+
+void iniciarValores()
+{
+    // Inicializar todos los valores
+	infinito = 1e30;
+	atendidos = 0;
+	inicio_ocio = 0.0;
+	acum_cola = 0.0;
+	reloj = 0;
+	tultsuc = reloj;
+	servidor_libre = true;
+	encola = 0;
+	tiempo_llegada = reloj + generaLlegada(tLlegada);
+	tiempo_salida = infinito;
+	ocio = 0.0;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+    {
+		tLlegada = 9;
+		tServicio = 6;
+		total_a_atender = 10000;
+		numSimul = 100;
+	}
+    else if (argc == 3)
+    {
+        tLlegada = atof(argv[1]);
+        tServicio = atof(argv[2]);
+        total_a_atender = 10000;
+        numSimul = 100;
+    }
+    else if (argc == 4)
+    {
+        tLlegada = atof(argv[1]);
+        tServicio = atof(argv[2]);
+        total_a_atender = atoi(argv[3]);
+		numSimul = 100;
+	}
+    else if (argc == 5)
+    {
+		tLlegada = atof(argv[1]);
+        tServicio = atof(argv[2]);
+        total_a_atender = atoi(argv[3]);
+        numSimul = atoi(argv[4]);
+	}
+    else
+    {
+        cerr << "Uso incorrecto del programa" << endl;
+        cerr << "Uso correcto: ./" << argv[0] << " <t. llegada> <t servicio> [<num clientes> [<num simulaciones>]]" << endl;
+		exit(1);
+	}
+
+    double tiempoTotal = 0.0, totalEnCola = 0.0, totalOcio = 0.0;
+
+    // Inicializar semilla
+	srand(time(NULL));
+
+    cout << "% T. ocio servidor, Num. medio clientes cola, Num. total clientes, T. total ejecución" << endl;
+
+	for(int i = 0; i < numSimul; i++)
+    {
+        // Iniciar valores
+		iniciarValores();
+
+		high_resolution_clock::time_point tIni, tFin;
+
+        // Obtener tiempo inicial
+		tIni = high_resolution_clock::now();
+
+		while(atendidos < total_a_atender)
+        {
+			if (reloj == tiempo_llegada)
+            {
+				tiempo_llegada = reloj + generaLlegada(tLlegada);
+                
+				if (servidor_libre)
+                {
+					servidor_libre = false;
+					tiempo_salida = reloj + generaServicio(tServicio);
+					ocio += reloj - inicio_ocio;
+				}
+                else
+                {
+					acum_cola += (reloj-tultsuc)*encola;
+					tultsuc = reloj;
+					encola++;
+				}
+			}
+
+			if (reloj == tiempo_salida)
+            {
+				atendidos++;
+
+				if (encola > 0)
+                {
+					acum_cola += (reloj-tultsuc)*encola;
+					tultsuc = reloj;
+					encola--;
+					tiempo_salida = reloj + generaServicio(tServicio);
+				}
+                else
+                {
+					servidor_libre = true;
+					inicio_ocio = reloj;
+					tiempo_salida = infinito;
+				}
+			}
+
+			reloj++;
+		}
+
+        tFin = high_resolution_clock::now();
+
+        // Calcular valores estadisticos
+        double porcent_ocio = ocio*100/reloj;
+        double media_encola = acum_cola/reloj;
+        double tiempoIter = duration_cast<duration<double>>(tFin-tIni).count();
+
+        tiempoTotal += tiempoIter;
+        totalEnCola += media_encola;
+        totalOcio += porcent_ocio;
+		
+        cout << porcent_ocio << " " << media_encola << " " << total_a_atender << " " << tiempoIter << endl;
+	}
+
+    double tiempoMedio = tiempoTotal / numSimul,
+           colaMedia = totalEnCola / numSimul,
+           mediaOcio = totalOcio / numSimul;
+
+    cout << "\nTiempo medio de ejecución: " << tiempoMedio << " seg." << endl;
+    cout << "Numero medio de clientes en cola: " << colaMedia << endl;
+    cout << "Tiempo medio del servidor en ocio: " << mediaOcio << endl;
+
+	if (tServicio < tLlegada)
+    {
+		float p = (float)(tServicio/tLlegada);
+		float q = (p*p)/(1-p);
+		float pto = 100*(1-p);
+        cout <<  endl << "p = tServicio / tLlegada = " << p << endl;
+        cout << "Q(n) = (p*p)/(1-p) = " << q << endl;
+        cout << "PTO = 100*(1-p) = " << pto << endl;
+	}
+
+	return 0;
+}
